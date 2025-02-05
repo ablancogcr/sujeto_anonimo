971b6f9e019df135de9752f1f0939b374b42b0d3
Andres B <andresblancog@gmail.com>
2025-02-05 10:58:16 -0600

    Fix libGL missing + remove toml

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+# Install system dependencies (for OpenCV & Gunicorn)
+apt-get update && apt-get install -y libgl1-mesa-glx
+
+# Install Python dependencies
 pip install --no-cache-dir -r requirements.txt
+
+# Start the server
 gunicorn -w 4 -b 0.0.0.0:5000 app:app
-
-
